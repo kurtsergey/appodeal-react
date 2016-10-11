b649a0df3b98b2b89846977190e790b115f083df
Tamplier <araman777@gmail.com>
2016-10-11 10:10:56 +0700

    ios appodeal initial files added

---
+++ ios/Appodeal.h
@@ -0,0 +1,7 @@
+
+#import "RCTBridgeModule.h"
+
+@interface Appodeal : NSObject <RCTBridgeModule>
+
+@end
+  
---
+++ ios/Appodeal.m
@@ -0,0 +1,13 @@
+
+#import "Appodeal.h"
+
+@implementation Appodeal
+
+- (dispatch_queue_t)methodQueue
+{
+    return dispatch_get_main_queue();
+}
+RCT_EXPORT_MODULE()
+
+@end
+  
